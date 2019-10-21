984847ae3be943b9830d87d12e8ebcad5cb625ab
asaf92 <asaf92@gmail.com>
2019-10-21 14:42:19 +0300

    Added strings constants class

---
+++ Assets/Scripts/Presentation/UI/Constants/HudMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Presentation.UI.Constants
+{
+    public static class HudMessages
+    {
+        public static string TimeStringFormat { get; } = @"mm\:ss\.ff"; // This will show one minute, two seconds and three ms as 01:02.03
+        public static string NewRecordString { get; } = "NEW RECORD!";
+    }
+}
--- Assets/Scripts/Presentation/UI/RaceTimerPanelController.cs
+++ Assets/Scripts/Presentation/UI/RaceTimerPanelController.cs
@@ -3,12 +3,12 @@ using Game.Constants;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using Presentation.UI.Constants;
 
 namespace Presentation.UI
 {
     public class RaceTimerPanelController : MonoBehaviour
     {
-        private const string TimeStringFormat = @"mm\:ss\.ff";
         [SerializeField]
         private GameObject _raceManagerGameObject;
         [SerializeField]
@@ -35,6 +35,6 @@ namespace Presentation.UI
         }
 
         private IRaceManager GetRaceManager() => _raceManagerGameObject.GetComponent<IRaceManagerComp>().RaceManager ?? throw new ArgumentNullException("Race manager not found");
-        private void UpdateTimerText() => _raceTimerTimeText.text = _currentRaceTime.ToString(TimeStringFormat) + Environment.NewLine + _bestTime.ToString(TimeStringFormat);
+        private void UpdateTimerText() => _raceTimerTimeText.text = _currentRaceTime.ToString(HudMessages.TimeStringFormat) + Environment.NewLine + _bestTime.ToString(HudMessages.TimeStringFormat);
     }
 }
