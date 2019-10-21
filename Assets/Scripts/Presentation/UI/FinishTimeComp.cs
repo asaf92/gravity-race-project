9c659b28f365e0e8f70845b2d64df5714d188431
asaf92 <asaf92@gmail.com>
2019-10-21 14:43:42 +0300

    Added finish time message

---
+++ Assets/Scripts/Presentation/UI/FinishTimeComp.cs
@@ -0,0 +1,39 @@
+﻿using Game.Race;
+using Presentation.UI.Constants;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Presentation.UI
+{
+    internal class FinishTimeComp : MonoBehaviour, IFinishTimeComp
+    {
+        [SerializeField] private GameObject _raceFinishTimeTextGameObject;
+        private Text _raceFinishTimeText;
+
+        [SerializeField] private GameObject _raceManagerObject;
+        private IRaceManager _raceManager;
+
+        private FinishTimeController _controller;
+
+        void Start()
+        {
+            _raceManager = _raceManagerObject.GetComponent<IRaceManagerComp>().RaceManager;
+            _controller = new FinishTimeController(this, _raceManager);
+            _raceFinishTimeText = _raceFinishTimeTextGameObject.GetComponent<Text>();
+            _raceFinishTimeText.enabled = false;
+        }
+
+        public void ShowFinishTime(TimeSpan finishTime, bool newRecord = false)
+        {
+            var finishTimeDisplayText = finishTime.ToString(HudMessages.TimeStringFormat);
+            if (newRecord)
+            {
+                finishTimeDisplayText += $"{Environment.NewLine}{HudMessages.NewRecordString}";
+            }
+
+            _raceFinishTimeText.text = finishTimeDisplayText;
+            _raceFinishTimeText.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Presentation/UI/FinishTimeController.cs
@@ -0,0 +1,24 @@
+﻿using Game.Race;
+using Game.Race.Events;
+using System;
+
+namespace Presentation.UI
+{
+    internal class FinishTimeController
+    {
+        private readonly IFinishTimeComp _component;
+        private IRaceManager _raceManager;
+
+        public FinishTimeController(IFinishTimeComp component, IRaceManager raceManager)
+        {
+            _component = component ?? throw new ArgumentNullException(nameof(component));
+            _raceManager = raceManager ?? throw new ArgumentNullException(nameof(raceManager));
+            _raceManager.RaceFinished += OnRaceFinished;
+        }
+
+        private void OnRaceFinished(object sender, RaceFinishedEventArgs e)
+        {
+            _component.ShowFinishTime(e.FinishTime, e.IsNewRecord);
+        }
+    }
+}
