c8e587601da439c7151becf0a5cd462664f97e09
asaf92 <asaf92@gmail.com>
2019-11-15 16:35:40 +0200

    Fixed high-score menu

---
+++ Assets/Scripts/Presentation/Main Menu/HighScoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using Game.Constants;
+using Presentation.UI.Constants;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class HighScoreController : MonoBehaviour
+{
+    private Text _highScoreText;
+
+    void OnEnable()
+    {
+        try
+        {
+            _highScoreText = GetComponent<Text>();
+
+            var highScore = TimeSpan.FromSeconds(PlayerPrefs.GetFloat(SettingsKeys.HighScore))
+                .ToString(HudMessages.TimeStringFormat);
+
+            _highScoreText.text = highScore;
+            Debug.Log($"Set highScore to {highScore}");
+        }
+        catch(Exception exception)
+        {
+            Debug.LogError($"Error in high-score screen:\n{exception.Message}");
+        }
+    }
+}
